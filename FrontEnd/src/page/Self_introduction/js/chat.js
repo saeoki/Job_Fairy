0af34d178469caea65496050eef8a27d64b77f08
김범수 <kbsu@MacBook-Air.local>
2024-04-10 17:57:28 +0900

    kbs gpt api success

--- FrontEnd/src/page/Self_introduction/js/chat.js
+++ FrontEnd/src/page/Self_introduction/js/chat.js
@@ -1,16 +1,28 @@
-async function apiCall(){
-    const { Configuration, OpenAIApi } = require("openai");
-    
-    const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-    });
-    const openai = new OpenAIApi(configuration);
-    
-    const completion = await openai.createCompletion({
-        model: "gpt-3.5-turbo",
-        prompt: "파이썬 자바",
-        });
-    }
-    
+import OpenAI from "openai";
 
-export default apiCall
+const openai = new OpenAI({
+    apiKey: process.env.REACT_APP_OPEN_API_KEY,
+    dangerouslyAllowBrowser: true,
+});
+
+export const CallGPT = async () => {
+    console.log(">> Call GPT")
+    
+    const completion = await openai.chat.completions.create({
+        messages: [{ 
+            "role": "system",
+            "content": "개발자 자기소개서 초안을 작성해주는 컨설턴트이다. 입력한 키워드에 맞춰 초안을 작성하는 업무를 담당한다.\n작성할 내용은 200자 이내로 마무리해야한다."
+        }],
+        model: "gpt-3.5-turbo",
+        temperature: 1,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+    });
+
+    const responseData = completion.choices[0]
+    console.log(responseData.message.content);
+
+    return responseData.message.content;
+}
