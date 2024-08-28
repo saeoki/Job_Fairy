cd51ac7c64f690e2b1bc6480e21cb92cca080638
정세욱 <sw000620@gmail.com>
2024-08-28 23:05:14 +0900

    데이터 추출, 정제, GCS업로드, BigQuery 벌크

--- Data_ETL/saramin_api.py
+++ Data_ETL/saramin_api.py
@@ -28,6 +28,10 @@ import os
 import time
 import random
 
+from google.cloud import bigquery
+from google.cloud.bigquery import SourceFormat
+
+
 load_dotenv('/')
 access_key=os.getenv('SARAMIN_API_KEY')
 
@@ -59,7 +63,7 @@ initial_data = fetch_jobs(start=0, count=1)
 print(initial_data)
 
 
-
+# 여기서 데이터 추출을 여러번 나눠 분할을 하든 뭘 하든 하나의 최종 파일로 만들어냄 (dag를 여기서 분할)
 if initial_data is not None and 'jobs' in initial_data and 'total' in initial_data['jobs']:
     total_results = int(initial_data['jobs']['total'])
     # total_results = 500
@@ -67,6 +71,8 @@ if initial_data is not None and 'jobs' in initial_data and 'total' in initial_da
 
     today_date = datetime.now().strftime("%Y%m%d")
     local_file = f'all_jobs_saramin_{today_date}.json'
+
+
 
     with open(local_file, 'w', encoding='utf-8') as f :
         f.write('[')
@@ -76,12 +82,13 @@ if initial_data is not None and 'jobs' in initial_data and 'total' in initial_da
     #max count is 110
     for start in range(0, total_results, max_count) :
         data = fetch_jobs(start=start, count=max_count)
+
         if data is not None and 'jobs' in data and 'job' in data['jobs']:
             jobs = data['jobs']['job']
             if not jobs : # job리스트에 데이터가 없을 경우
-                print(f"Warning: job리스트에 데이터가 없습니다. 종료하진 않고 알려만 드립니다 ㅋ.")
-                print(jobs)
-                # break
+                print(f"Warning: job리스트에 데이터가 없습니다.")
+                received_count = 0
+                break
 
             received_count = len(jobs)
 
@@ -90,7 +97,7 @@ if initial_data is not None and 'jobs' in initial_data and 'total' in initial_da
                     f.write(',')
                 else :
                     first_batch = False
-                f.write(json.dumps(jobs, ensure_ascii=False)[1:-1]) # 파이썬 구조의 객체를 JSON 형식으로
+                f.write(json.dumps(jobs, ensure_ascii=False)[1:-1]) # 파이썬 구조의 객체를 JSON 문자열로
             
             print(f"{total_results}중의 {start}번 째 실행 완료.")
 
@@ -101,11 +108,7 @@ if initial_data is not None and 'jobs' in initial_data and 'total' in initial_da
         else :
             print(f"Warning : {start}번째 응답에서 데이터가 없습니다.")
             break
-
         time.sleep(2.5)
-
-
-
 
     with open(local_file, 'a', encoding='utf-8') as f :
         f.write(']')
@@ -117,9 +120,61 @@ else :
 
 
 
+# JSON to NDJSON
+with open(local_file, 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+with open(local_file, 'w', encoding='utf-8') as f:
+    for entry in data:
+        json.dump(entry, f, ensure_ascii=False) # ensure_ascii옵션 설정을 안하면 한글이 다른 문자열로 변환됨
+        f.write('\n')
+
+
 # Upload file to GCS
 bucket_name = 'jobfairy-gcs'
 destination_blob_name = local_file
 upload_to_gcs(bucket_name, local_file, destination_blob_name)
 
 
+
+
+
+
+# GCS to BigQuery
+
+# 설정 변수
+project_id = "eloquent-vector-423514-s2"
+dataset_id = "raw_data"
+table_id = "all_job_posting" # 테이블이 존재하지 않으면 해당 이름으로 자동 생성
+gcs_bucket_name = bucket_name
+gcs_file_name = destination_blob_name
+gcs_uri = f"gs://{gcs_bucket_name}/{gcs_file_name}"
+
+# BigQuery 클라이언트 생성
+bigquery_client = bigquery.Client(project=project_id)
+
+# LoadJobConfig 생성
+job_config = bigquery.LoadJobConfig(
+    source_format=SourceFormat.NEWLINE_DELIMITED_JSON,
+    autodetect=True,  # 자동으로 스키마 감지 (옵션)
+    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE
+)
+
+# 데이터셋과 테이블 참조 생성
+table_ref = f"{project_id}.{dataset_id}.{table_id}"
+
+# GCS에서 BigQuery로 데이터 로드
+load_job = bigquery_client.load_table_from_uri(
+    gcs_uri,
+    table_ref,
+    job_config=job_config,
+)  # API 요청
+
+print(f"Loading data from {gcs_uri} into {table_ref}")
+
+# 작업 완료 대기
+load_job.result()
+
+# 로드된 테이블 정보 확인
+destination_table = bigquery_client.get_table(table_ref)
+print(f"Loaded {destination_table.num_rows} rows into {table_ref}.")
