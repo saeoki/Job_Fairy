38dc64a0537682c8425cdcd9d985deaae4fa483f
정세욱 <sw000620@gmail.com>
2024-11-12 16:17:08 +0900

    로그 없애기, 태블로 창 크기 키움

--- FrontEnd/src/page/Recruitment/js/DashBoardBody.js
+++ FrontEnd/src/page/Recruitment/js/DashBoardBody.js
@@ -32,7 +32,7 @@ export default function DashBoardBody() {
         style={{
           position: "relative",
           width: "100%",
-          maxWidth: "800px",
+          maxWidth: "1200px",
           paddingBottom: "56.25%",
           height: 0,
         }}
